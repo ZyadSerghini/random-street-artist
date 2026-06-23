dc8418c3af0f58c596cae7f41e8525f650cd2fb7
Zyad Serghini <z.serghini@aui.ma>
2026-06-23 12:44:32 +0100

    Cleaned structure

--- street_artist/__main__.py
+++ street_artist/__main__.py
@@ -1,23 +1,20 @@
 from . import street, ground
 import turtle as tt
-import random
 
 tt.speed(0)
 
 def main():
     """Draws a street with 4 randomly generated buildings."""
     
-    y_ground = random.randint(-375, -50)
+    y_ground = -150
     ground.ground(y_ground)
     for i in range(4):
         street.building(-390 + i*240, y_ground)
     
     #moving the cursors aside to avoid it covering the buildings.
-    tt.setposition(0, 300)
+    # tt.setposition(0, 300)
     
 if __name__ == "__main__":
     main()
-    canvas = tt.getcanvas()
-    canvas.postscript(file="street.eps")
     tt.done()
     
